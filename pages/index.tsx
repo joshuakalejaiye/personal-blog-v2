2fb75f0e10d650449c398223a766922b0e693bfb
Adedayo (Joshua) Kalejaiye <joshua.s.kalejaiye@gmail.com>
2021-12-31 15:00:53 +0000

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Modified Base for Creating Next.js Applications
-        </h1>
+        <h1 className={styles.title}>Video et Games</h1>
       </main>
 
       <footer className={styles.footer}>
